fbb5f74dd33dd3f8a8bc298ec6bb18f9f4db888b
maxischiavina <123894494+maxischiavina@users.noreply.github.com>
2023-05-06 18:04:42 +0000

    ultimo commit?

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -5,39 +5,56 @@ import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+	const [dato, setDato] = useState("");
+	const [listaDatos, setListaDatos] = useState([]);
 
-const [dato, setDato] = useState("");
-const [listaDatos, setListaDatos] = useState([]);
-const pendiente = listaDatos.length
 
-function handleChange(event){
-setDato(event.target.value)
-};
+	// const pendiente = listaDatos.length
 
-function handleSubmit(event){
-	event.preventDefault();
-	setListaDatos([...listaDatos, dato]);
-	setDato("");
-}
+	function pendiente(){
+		if (listaDatos.length >= 1) return (listaDatos.length + " Tareas pendientes")
+		else return ("No hay tareas pendientes, agregar tarea")
+	}
+	
+	function handleChange(event){
+	setDato(event.target.value)
+	};
+	
+	function handleSubmit(event){
+		event.preventDefault();
+		setListaDatos([...listaDatos, dato]);
+		setDato("");
+	}
+	
+	function borrarDato(nombre){
+	const nuevaLista = listaDatos.filter((item)=> item !== nombre);
+	setListaDatos(nuevaLista);
+	}
 
-function borrarDato(nombre){
-const nuevaLista = listaDatos.filter((item)=> item !== nombre);
-setListaDatos(nuevaLista);
-}
+
 	return (
-		<div className="bg-dark d-flex align-items center justify-content-center">
-		<div className="bg-white w-50 align-items center" >
+		<div className="bg-dark d-flex align-items center justify-content-center " style={{ height: 500 }}>
+		<div className="bg-white w-50 align-items center m-2" style={{ height: 450 }}>
 			<h1 className="text-center justify-content-center fs-1">Todos</h1>
 
 
 			<form onSubmit={handleSubmit}>
-  <div className="m-0"  style={{ height: 300 }}>
-    <label htmlFor="datos" className="form-label"></label>
+  <div className="m-0"  style={{ height: 250 }}>
+    
     <input type="text" className="form-control p-0 m-0" id="datos" aria-describedby="emailHelp" onChange={handleChange} value={dato} required />
+  
+	<ul className="p-0 m-0" style={{ listStyle: "none" }}>
+  {listaDatos.map((item, index)=> (
+    <li className="border-top border-bottom m-0 p-0" key={index}>
+      <div className="d-flex justify-content-between align-items-center">
+        <span>{item}</span>
+        <button type="button" className="btn " onClick={() => borrarDato(item)}>x</button>
+      </div>
+    </li>
+  ))}
+</ul>
+  <p className="p-0 m-0 border">{pendiente()}</p>
   </div>
-  <ul className="p-0" style={{ listStyle: "none" }}>{listaDatos.map((item, index)=> (<li className="border-top border-bottom m-0 p-0 d-flex" key={item.id}>{item} <span className="border-0 bg-white justify-content-end" type="button" onClick={()=> borrarDato(item)}>X</span></li>))}</ul>
-  <p className="p-0 m-0 border">{pendiente} Items Left</p>
-  <button type="submit" className="btn btn-primary">Submit</button>
 </form>
 </div>
 		</div>
